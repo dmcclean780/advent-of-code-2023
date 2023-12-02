4be3f2fad0dd79c23dc731541c763b6d0da9c486
Daniel McClean <dmcclean780@gmail.com>
2023-12-02 12:16:24 +0000

    Completed Day 2 part 1

---
+++ Day2/part1.js
@@ -0,0 +1,83 @@
+const fs = require('node:fs');
+var input
+try {
+  const data = fs.readFileSync('/home/daniel/Documents/advent-of-code-2023/advent-of-code-2023/Day2/input.txt', 'utf8');
+  input=data.split("\n");
+} catch (err) {
+  console.error(err);
+}
+
+function trimInput(inputList){
+  for(var i=0; i<inputList.length; i++){
+    str=inputList[i];
+    str = str.substring(str.indexOf(":") + 2);
+    inputList[i]=str;
+  }
+  return inputList
+}
+
+function splitInput(inputList){
+  var splitInputList=[]
+  var finalInput=[[]];
+  for(var i=0; i<inputList.length; i++){
+    splitInputList[i]=inputList[i].split("; ");
+  }
+  for(var i=0; i<inputList.length; i++){
+    finalInput[i]=new Array(splitInputList[i].length)
+    for(var j=0; j<splitInputList[i].length; j++){
+      finalInput[i][j]=splitInputList[i][j].split(", ");
+    }
+  }
+  return finalInput;
+}
+
+function getColourNumbers(inputList){
+  var colourAmount=[]
+  for(var i=0; i<inputList.length; i++){
+    colourAmount[i]=new Array(inputList[i].length);
+    for(var j=0;j<inputList[i].length; j++){
+      colourAmount[i][j]=[0,0,0];
+      for(var k=0; k<inputList[i][j].length; k++){
+        var colour=inputList[i][j][k].split(" ")
+        if(colour[1]=="red"){
+          colourAmount[i][j][0]=colour[0];
+        }
+        if(colour[1]=="blue"){
+          colourAmount[i][j][1]=colour[0];
+        }
+        if(colour[1]=="green"){
+          colourAmount[i][j][2]=colour[0];
+        }
+      }
+    }
+  }
+  return colourAmount;
+}
+
+function checkValid(colours){
+  var valid=true;
+  var sum=0;
+  for(var i=0; i<colours.length; i++){
+    for(var j=0; j<colours[i].length; j++){
+        if(Number(colours[i][j][0])>12){
+          valid=false;
+        }
+        if(Number(colours[i][j][1])>14){
+          valid=false;
+        }
+        if(Number(colours[i][j][2])>13){
+          valid=false;
+        }
+    }
+    if(valid){
+      sum=sum+i+1
+    }
+    valid=true
+  }
+  console.log(sum);
+}
+
+input=trimInput(input);
+input=splitInput(input);
+var colours=getColourNumbers(input);
+checkValid(colours);
